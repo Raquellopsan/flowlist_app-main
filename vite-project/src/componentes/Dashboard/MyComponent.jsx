963e8c12a6c85f2a88ecb6c5c9998dee676625ed
Raquel <raquellopezsancheez@gmail.com>
2025-04-27 21:52:36 +0200

    ok

--- vite-project/src/componentes/Dashboard/MyComponent.jsx
+++ vite-project/src/componentes/Dashboard/MyComponent.jsx
@@ -1,41 +1,41 @@
-// // src/componentes/dashboard/MyComponent.js
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
+// src/componentes/dashboard/MyComponent.js
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-// const MyComponent = () => {
-//   const [data, setData] = useState(null);
-//   const [error, setError] = useState(null);
+const MyComponent = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-//   useEffect(() => {
-//     const apiUrl = process.env.REACT_APP_API_URL;
+  useEffect(() => {
+    const apiUrl = process.env.REACT_APP_API_URL;
 
-//     // Hacer la solicitud GET usando axios
-//     axios
-//       .get(`${apiUrl}/api/endpoint`)
-//       .then((response) => {
-//         setData(response.data); // Guardar los datos en el estado
-//         console.log("Datos obtenidos:", response.data);
-//       })
-//       .catch((error) => {
-//         setError(error); // Guardar el error en el estado
-//         console.error("Error al obtener los datos:", error);
-//       });
-//   }, []); // El array vacío [] asegura que el efecto se ejecute solo una vez, cuando el componente se monte
+    // Hacer la solicitud GET usando axios
+    axios
+      .get(`${apiUrl}/api/endpoint`)
+      .then((response) => {
+        setData(response.data); // Guardar los datos en el estado
+        console.log("Datos obtenidos:", response.data);
+      })
+      .catch((error) => {
+        setError(error); // Guardar el error en el estado
+        console.error("Error al obtener los datos:", error);
+      });
+  }, []); // El array vacío [] asegura que el efecto se ejecute solo una vez, cuando el componente se monte
 
-//   if (error) {
-//     return <div>Error: {error.message}</div>;
-//   }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
-//   if (!data) {
-//     return <div>Cargando...</div>;
-//   }
+  if (!data) {
+    return <div>Cargando...</div>;
+  }
 
-//   return (
-//     <div>
-//       <h1>Datos obtenidos</h1>
-//       <pre>{JSON.stringify(data, null, 2)}</pre>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h1>Datos obtenidos</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
 
-// export default MyComponent;
+export default MyComponent;
